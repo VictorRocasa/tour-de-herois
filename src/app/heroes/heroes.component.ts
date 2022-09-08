e3a3b4bbbb0e69c31bffb7d9f12b34993ca6bc37
Victor <victorcapobianco01@yahoo.com.br>
2022-09-08 09:44:05 -0300

    Implementado listar herois

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,6 +8,8 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+
+  heroes = HEROES;
 
   hero: Hero = {
     id:1,
@@ -17,6 +20,11 @@ export class HeroesComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+selectedHero?: Hero;
+onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+}
 
 }
 
